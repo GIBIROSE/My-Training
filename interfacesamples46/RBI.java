c4251e18d9128c2a7162ae53e540f0a4a56a2a3d
GIBIROSE <gibirose.rose@gmail.com>
2022-03-15 12:31:32 +0530

    interfaceprogram

---
+++ interfacesamples46/RBI.java
@@ -0,0 +1,9 @@
+package training.interfacesamples46;
+
+public interface RBI {
+	public static final float INTERESTRATE=5.77f;
+	
+	public abstract void recurringDeposit(int amount, int duration);
+	
+
+}
